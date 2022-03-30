6f45df93e9e2ad8a29fc8cad79ac3008e603f7bb
HENRY-KC <henrykc24@yahoo.com>
2022-03-30 18:17:46 +0100

    Create initital Cart page

---
+++ src/pages/Cart.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export class Cart extends PureComponent {
+  render() {
+    return (
+      <div>Cart</div>
+    );
+  }
+}
+
+export default Cart;
