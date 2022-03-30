98965c435b5d66e72b32e86e4c4227e326df06dd
HENRY-KC <henrykc24@yahoo.com>
2022-03-30 20:23:00 +0100

    Test cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { PureComponent } from 'react';
 import Header from './components/Header';
 // import Category from './pages/Category';
-import Detail from './pages/Details';
+// import Detail from './pages/Details';
+import CartPage from './pages/Cart';
 import './App.css';
 
 class App extends PureComponent {
@@ -9,7 +10,7 @@ class App extends PureComponent {
     return (
       <div>
         <Header />
-        <Detail />
+        <CartPage />
 
       </div>
     );
